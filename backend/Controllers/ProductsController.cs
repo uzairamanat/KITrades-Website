9c4f11127d006acea6a9902e77067f62e43f7183
uzairamanat <uzair.amanat@hotmail.com>
2025-10-03 18:25:52 +0100

    setup frontend and backend

---
+++ backend/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return Ok(new[] { "Apples", "Bananas", "Carrots" });
+    }
+}
